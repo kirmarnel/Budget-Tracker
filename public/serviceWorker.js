b71986d8e0138290f2eb07fc18305b964d1c4e4d
Kirsten Nelson <kirmarnel@gmail.com>
2021-06-16 20:13:03 -0700

    deploying

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -4,7 +4,8 @@ const FILES_TO_CACHE = [
     "/",
     "/index.html",
     "index.js",
-    "manifest.webmanifest",
+    "/db.js",
+    "/manifest.webmanifest",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png"
   ];
